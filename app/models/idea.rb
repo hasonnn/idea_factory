40b31ad3625b2848bbb2f07793e0a93a11917891
Jason Chan <jchan321738@gmail.com>
2021-05-06 14:20:23 -0700

    Create Like Model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -5,4 +5,7 @@ class Idea < ApplicationRecord
     belongs_to :user
 
     has_many :reviews, dependent: :destroy
+
+    has_many :likes, dependent: :destroy
+    has_many :likers, through: :likes, source: :user
 end
