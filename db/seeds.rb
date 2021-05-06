1bb6fa9efd006354fad5d7fa7b6176386846907c
Jason Chan <jchan321738@gmail.com>
2021-05-06 09:37:22 -0700

    Create Idea Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Idea.destroy_all
+
+50.times do
+    created_at = Faker::Date.backward(days:365 * 5)
+
+    Idea.create(
+        title: Faker::GreekPhilosophers
+        body: Faker::Lorem.sentence(word_count: 55)
+    )
+end
+
+ideas = Idea.all
+
+puts "Generated #{ideas.count} ideas!"
