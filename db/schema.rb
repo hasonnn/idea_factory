24d8519e65be2d78fd270568f6d02428fc3ffced
Jason Chan <jchan321738@gmail.com>
2021-05-06 13:09:18 -0700

    Create Review Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_180121) do
+ActiveRecord::Schema.define(version: 2021_05_06_200635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2021_05_06_180121) do
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "body"
+    t.bigint "idea_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["idea_id"], name: "index_reviews_on_idea_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -35,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_05_06_180121) do
   end
 
   add_foreign_key "ideas", "users"
+  add_foreign_key "reviews", "ideas"
 end
